68e16f4549598e005f0542489b88ab5bc31c91cb
“Russel <russeldanielpaul@gmail.com>
2025-06-18 22:46:05 +0530

    first commit with tailwind working

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl text-blue-500 font-bold">
+          
+      Welcome</Text>
+    </View>
+  );
+}
